50ee43d9266dac05ff757835579a6e536bdf76c6
mannatmohapatra933 <mannatmohapatra76@gmail.com>
2026-04-18 13:57:35 +0530

    Debug: Log environment variables on startup

--- java-backend/src/main/java/com/example/demo/DemoApplication.java
+++ java-backend/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("DEBUG: DATABASE_URL = " + System.getenv("DATABASE_URL"));
+        System.out.println("DEBUG: DB_URL = " + System.getenv("DB_URL"));
         SpringApplication.run(DemoApplication.class, args);
     }
 
