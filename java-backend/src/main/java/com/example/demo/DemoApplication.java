0e45a60bbd36fd385d52f492f6b7132815ff7ad3
mannatmohapatra933 <mannatmohapatra76@gmail.com>
2026-04-18 14:05:12 +0530

    Debug: Log PG explicit variables

--- java-backend/src/main/java/com/example/demo/DemoApplication.java
+++ java-backend/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,10 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("DEBUG: DATABASE_URL = " + System.getenv("DATABASE_URL"));
-        System.out.println("DEBUG: DB_URL = " + System.getenv("DB_URL"));
+        System.out.println("DEBUG: PGHOST = " + System.getenv("PGHOST"));
+        System.out.println("DEBUG: PGPORT = " + System.getenv("PGPORT"));
+        System.out.println("DEBUG: PGDATABASE = " + System.getenv("PGDATABASE"));
+        System.out.println("DEBUG: PGUSER = " + System.getenv("PGUSER"));
         SpringApplication.run(DemoApplication.class, args);
     }
 
